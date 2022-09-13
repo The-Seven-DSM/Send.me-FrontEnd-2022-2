084539adec05042c0d2eb907fc1a269272461d9c
Victor dos Santos Salles <victorgamesbr091@gmail.com>
2022-09-13 15:58:34 -0300

    finalização do formulário

--- src/components/Table/table.js
+++ src/components/Table/table.js
@@ -4,8 +4,14 @@ import Header from "../Header/header";
 // import { FaCheckCircle } from "react-icons/fa";
 // import { FaRegCheckCircle } from "react-icons/fa";
 
+
 const Table = () => {
 
+    const Link = () => {
+       
+        // eslint-disable-next-line no-restricted-globals
+        location.href = `/editor`
+    }
     return (
         <>
         <Header />
@@ -13,66 +19,69 @@ const Table = () => {
             <div className="search">
               <input type="text" placeholder="Nome"/>
               <select>
-                    <option value="0">Selecione</option>
-                    <option value="1">Teste1</option>
-                    <option value="2">Teste2</option>
+                    <option value="0">Selecione o Caderno</option>
+                    <option value="1">Cidade</option>
+                    <option value="2">Executivo I</option>
+                    <option value="3">Executivo II</option>
                 </select>
             </div>
             <div className="table-section">
             <table>
                 <thead>
                     <tr>
+                        
                         <th>Nome</th>
                         <th>Email</th>
                         <th>Caderno</th>
                         <th>Status</th>
+                        
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Wallace da Silva</td>
+                    <tr onClick={Link}> 
+                        <td>Wallace da Silva </td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Executivo I</td>
                         <td><Button /></td>
-                        
+                    
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Executivo II</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Cidade</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Executivo I</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Executivo I</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Executivo II</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Caderno</td>
                         <td><Button /></td>
                     </tr>
-                    <tr>
+                    <tr onClick={Link}>
                         <td>Wallace da Silva</td>
                         <td>wllcfelipe@gmail.com</td>
                         <td>Caderno</td>
