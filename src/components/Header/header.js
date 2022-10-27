699e8d930739223f7919257bab6f5f7e656933bc
VictorSantos18 <victorgamesbr091@gmail.com>
2022-10-27 08:30:34 -0300

    navebar responsiva

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import {Component} from 'react'
 
-function Header() {
+class Header extends Component {
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({clicked: !this.state.clicked})
+  }
+  render(){
 
   return (
     <nav>
       <a className="logo-text" href="/home">Send.me</a>
-      <ul className="nav-list">
+      <ul id="nav-list" className={this.state.clicked ? "#nav-list active" : "#nav-list"}>
         <li><a href="/Cadastro_Usuario">Cadastro de Usuários</a></li>
+        <li><a href="/Cadastro_BackOffice">Cadastro de BackOffice</a></li>
         <li><a href="/lista">Lista de Usuários</a></li>
       </ul>
+
+      <div className='menu-mobile' onClick={this.handleClick}>
+        <i id="bar" className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+      </div>
     </nav>
   )
 };
+}
 export default Header
