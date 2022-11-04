1376af5b83cc3d67d3081265f6225fefb9707870
Victor dos Santos Salles <victorgamesbr091@gmail.com>
2022-11-04 18:41:46 -0300

    atualização no cadastro do backoffice

--- src/components/cadastro_backoffice/cadastro_backoffice.js
+++ src/components/cadastro_backoffice/cadastro_backoffice.js
@@ -1,52 +1,78 @@
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/heading-has-content */
-import Header from '../Header/header'
-import Voltar from '../../assets/img/voltar.png'
+import React from 'react'
 
 const Cadastro_Backoffice = () => {
 
     return (
         <>
-            <Header />
-            <div className="background">
-                <div className="back-button-registration">
-                    <a href="/home"><img src={Voltar} alt="Voltar" /></a>
-                    <h3>Cadastro do BackOffice</h3>
-                </div>
-                <div className="container">
+            <main className="salve">
+
+                {/* <div className="cadastro">
                     <form action="/home">
                         <div className="detail">
 
                             <div className="input-box">
-                                <span>Nome</span>
+                                <p>Nome</p>
                                 <input name="nome" placeholder="Digite seu Nome Completo" required />
                             </div>
                             <div className="input-box">
-                                <span>CPF</span>
+                                <p>CPF</p>
                                 <input type="text" name="cpf" maxLength={12} placeholder="Ex: 000000000-00" required />
                             </div>
                             <div className="input-box">
-                                <span>Data de Nascimento</span>
+                                <p>Data de Nascimento</p>
                                 <input type="date" className="input-nascimento" name="datanascimento" required />
                             </div>
                             <div className="input-box">
-                                <span>Telefone</span>
+                                <p>Telefone</p>
                                 <input type='number' name="telefone" maxLength={11} placeholder="Ex: 12 00000-0000" required />
                             </div>
                             <div className="input-box">
-                                <span>Escola Vinculada</span>
+                                <p>Escola Vinculada</p>
                                 <input type='text' name="escola-vinculado" maxLength={11} placeholder="Digite o nome da escola em que está vinculado" required />
                             </div>
                             
                         </div>
 
                         <div className="button-registration">
+                            <a href='/'>Voltar</a>
                             <input type="submit" value="CADASTRAR" />
                         </div>
 
                     </form>
 
+                </div> */}
+
+                <div className="cadastro">
+                    <h3>Cadastro</h3>
+
+                    <div>
+                        <p>Nome</p>
+                        <input name="nome" placeholder="Digite seu Nome Completo" required />
+                    </div>
+                    <div>
+                        <p>CPF</p>
+                        <input type="text" name="cpf" maxLength={12} placeholder="Ex: 000000000-00" required />
+                    </div>
+                    <div>
+                        <p>Data de Nascimento</p>
+                        <input type="date" className="input-nascimento" name="datanascimento" required />
+                    </div>
+                    <div>
+                        <p>Telefone</p>
+                        <input type='number' name="telefone" maxLength={11} placeholder="Ex: 12 00000-0000" required />
+                    </div>
+                    <div>
+                        <p>Escola Vinculada</p>
+                        <input type='text' name="escola-vinculado" maxLength={11} placeholder="Digite o nome da escola em que está vinculado" required />
+                    </div>
+                    <button onClick={() => auth()}>Cadastrar</button>
+                    <p><a href="/">Voltar</a></p>
+
                 </div>
-            </div>
+
+            </main>
 
         </>
 
