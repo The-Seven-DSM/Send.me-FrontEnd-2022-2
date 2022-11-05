2d3c1cf9aff837d0c8bf5494f644f965c6bc716f
Valderi Douglas <96298784+ValderiDouglas@users.noreply.github.com>
2022-11-05 11:28:52 -0300

    Create backoffice, integração com backend

--- src/components/cadastro_backoffice/cadastro_backoffice.js
+++ src/components/cadastro_backoffice/cadastro_backoffice.js
@@ -1,9 +1,34 @@
 /* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/heading-has-content */
-import React from 'react'
+import Header from '../Header/header'
+import Voltar from '../../assets/img/voltar.png'
+import Axios from "axios";
+import React, { useState } from "react";
 
 const Cadastro_Backoffice = () => {
+    const [values, setValues] = useState([]);
+    const pega = (value) => {
+        setValues(preValue => ({
 
+            ...preValue,
+            [value.target.name]: value.target.value,
+
+        }))
+    }
+    const manda = () => {
+        Axios.post(`http://localhost:3001/create/backoffice`, {
+            nome: values.nome,
+            email: values.email,
+            senha: values.senha,
+            cpf: values.cpf,
+            datanascimento: values.datanascimento,
+            telefone: values.telefone,
+            escola: values.escolavinculado,
+        }
+        ).then(resp => {
+            // console.log(values);
+        });
+    }
     return (
         <>
             <main className="salve">
@@ -49,25 +74,34 @@ const Cadastro_Backoffice = () => {
 
                     <div>
                         <p>Nome</p>
-                        <input name="nome" placeholder="Digite seu Nome Completo" required />
+                        <input onChange={pega} name="nome" placeholder="Digite seu Nome Completo" required />
                     </div>
                     <div>
+                        <p>Email</p>
+                        <input onChange={pega} name="email" placeholder="Digite o Email" required />
+                    </div>
+                    <div>
+                        <p>Senha</p>
+                        <input onChange={pega} name="nome" placeholder="Digite a Senha" required />
+                    </div>
+
+                    <div>
                         <p>CPF</p>
-                        <input type="text" name="cpf" maxLength={12} placeholder="Ex: 000000000-00" required />
+                        <input onChange={pega} type="text" name="cpf" maxLength={12} placeholder="Ex: 000000000-00" required />
                     </div>
                     <div>
                         <p>Data de Nascimento</p>
-                        <input type="date" className="input-nascimento" name="datanascimento" required />
+                        <input onChange={pega} type="date" className="input-nascimento" name="datanascimento" required />
                     </div>
                     <div>
                         <p>Telefone</p>
-                        <input type='number' name="telefone" maxLength={11} placeholder="Ex: 12 00000-0000" required />
+                        <input onChange={pega} type='number' name="telefone" maxLength={12} placeholder="Ex: 12 00000-0000" required />
                     </div>
                     <div>
                         <p>Escola Vinculada</p>
-                        <input type='text' name="escola-vinculado" maxLength={11} placeholder="Digite o nome da escola em que está vinculado" required />
+                        <input onChange={pega} type='text' name="escolavinculado" maxLength={120} placeholder="Digite o nome da escola em que está vinculado" required />
                     </div>
-                    <button onClick={() => auth()}>Cadastrar</button>
+                    <button onClick={() => manda()}>Cadastrar</button>
                     <p><a href="/">Voltar</a></p>
 
                 </div>
